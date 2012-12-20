badff157614eb9fb4f46e65f467075bfa3fa4dab
ax003d <ax003d@gmail.com>
2012-12-21 00:56:46 +0800

    * get weibo timeline

--- inet.py
+++ inet.py
@@ -1,18 +1,43 @@
-import cmd
+from cmd import Cmd
+
+from weibo import APIClient
+
+from settings_ax003d import APP_KEY, APP_SECRET, CALLBACK_URL, TOKEN
 
 
-class INetManager(cmd.Cmd):
-    def init(self):
-        cmd.Cmd.init(self)
+class INet(Cmd):
+    def __init__(self):
+        Cmd.__init__(self)
+        self.client = APIClient(
+            app_key=APP_KEY, 
+            app_secret=APP_SECRET, 
+            redirect_uri=CALLBACK_URL)
 
-    def help_wb_new(self):
-        print "new weibo messages"
+    def do_quit(self, line):
+        exit(0)
+    
+    def do_login(self, line):
+        url = self.client.get_authorize_url()
+        print url
+        code = raw_input('code:')
+        print code
+        r = self.client.request_access_token(code)
+        access_token = r.access_token
+        expires_in = r.expires_in
+        print r
+        self.client.set_access_token(access_token, expires_in)
 
-    def do_wb_new(self, line):
-        print "new weibo"	
+    def do_tlogin(self, line):
+        r = TOKEN
+        access_token = r['access_token']
+        expires_in = r['expires_in']
+        self.client.set_access_token(access_token, expires_in)
+
+    def do_timeline(self, line):
+        print self.client.statuses.user_timeline.get()
 
 
 if __name__=='__main__':
-    inet = INetManager()
+    inet = INet()
     inet.cmdloop()
 
---
+++ settings.py
@@ -0,0 +1,4 @@
+APP_KEY = "weibo app key"
+APP_SECRET = "weibo app secret"
+CALLBACK_URL = 'weibo callback url'
+TOKEN = 'weibo token dict for test'
